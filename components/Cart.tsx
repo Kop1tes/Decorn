8f58fcb45bacc50ff6dac2af0a303779df6f3cea
Alexandr Prikhodko <prsasha0@gmail.com>
2026-08-28 10:17:59 +0300

    feat: calculate cart total

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -5,13 +5,19 @@ import { useCart } from "@/context/CartContext"
 export default function Cart() {
     const { cart, addToCart, removeFromCart } = useCart();
 
+    const total = cart.reduce((total, item) => {
+        total += item.product.price * item.quantity;
+
+        return total;
+    }, 0);
+
     return (
         <section>
             <h2>Корзина</h2>
             {cart.map((item) => (
                 <div key={item.product.id}>
                     <div>{item.product.title}</div>
-                    <div>Quantity: {item.quantity}</div>
+                    <div>Кiлькiсть: {item.quantity}</div>
                     <div className="flex gap-2">
                         <button
                             onClick={() => addToCart(item.product)}
@@ -26,7 +32,7 @@ export default function Cart() {
                             -
                         </button>
                     </div>
-                    
+                    <div>Разом: {total} ₴</div>
                 </div>
             ))}
         </section>
